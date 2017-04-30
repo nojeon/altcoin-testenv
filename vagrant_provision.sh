df39e42158fe8fa86ec996ce475e5f7be1de8cc4
Florian Maier <florian.maier@maibornwolff.de>
2017-04-30 23:50:26 +0200

    fix provisioning script

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -7,18 +7,22 @@ perl -pi -e 's@^\s*(deb(\-src)?)\s+http://us.archive.*?\s+@\1 mirror://mirrors.u
 # update repositories
 apt-get update
 
+# msttcorefonts hack
+echo ttf-mscorefonts-installer \
+   msttcorefonts/accepted-mscorefonts-eula \
+   select true \
+   | sudo debconf-set-selections
+
 # install the RNS windows wallet for illustration purposes 
 apt-get -y install wine-development winetricks unzip
 
-# cat <<'EOF' >> /usr/local/bin/runwallet
-!/bin/bash
+cat > /usr/local/bin/runwallet <<'EOF'
+#!/bin/bash
 winetricks settings win7
 wget https://github.com/RenosCoin/RenosCoin/releases/download/v1.0.0.0/renoscoin-qt.zip
 unzip renoscoin-qt.zip
 wine renoscoin-qt.exe
-fi
 EOF
-chmod u+x /usr/local/bin/runwallet
 
 # run the wallet after installation with the following command:
 echo "run the wallet after installation with the following command: /usr/local/bin/runwallet"
